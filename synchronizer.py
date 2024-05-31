dcdfbd4e5f8c732cbaafff392bbaf6de47e56f0e
Nam-Phuong Tran <namphuong@macbookpro.local>
2024-05-31 20:04:49 +0800

    remove redundant code

--- synchronizer.py
+++ synchronizer.py
@@ -1,17 +1,16 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import csv
 from dataset import ChunkedTimingDataset
 import glob
 
 
-class CNNPacketDetection(nn.Module):
+class CNNSynchronizer(nn.Module):
     def __init__(self, input_length, num_classes):
-        super(CNNPacketDetection, self).__init__()
+        super(CNNSynchronizer, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=9, kernel_size=3, stride=1)
         self.conv2 = nn.Conv1d(in_channels=9, out_channels=5, kernel_size=3, stride=1)
         self.flattened_size = 5 * (input_length - 4)  
@@ -44,31 +43,12 @@ eval_dataset = ChunkedTimingDataset(eval_y_files, eval_t_files)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 eval_loader = DataLoader(eval_dataset, batch_size=32, shuffle=False)
 
-# # Load the datasets from files
-# input_data = np.load('dataset_y.npy')
-# label_data = np.load('dataset_t.npy')
-
-# # Create a custom PyTorch Dataset class
-# class TimingDataset(Dataset):
-#     def __init__(self, inputs, labels):
-#         self.inputs = torch.tensor(inputs, dtype=torch.float32)
-#         self.labels = torch.tensor(labels, dtype=torch.float32)
-    
-#     def __len__(self):
-#         return len(self.inputs)
-    
-#     def __getitem__(self, idx):
-#         return self.inputs[idx], self.labels[idx]
-
-# # Create the dataset and DataLoader
-# timing_dataset = TimingDataset(input_data, label_data)
-# dataloader = DataLoader(timing_dataset, batch_size=32, shuffle=True)
 
 input_length = train_dataset.y_data.shape[1]  
 num_classes = train_dataset.t_data.shape[1]   
 
 # Initialize the model, define the loss function and the optimizer
-model = CNNPacketDetection(input_length, num_classes)
+model = CNNSynchronizer(input_length, num_classes)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
