b29e1fd84b2d771301f4e2e4e652e6768761f170
Nam-Phuong Tran <namphuong@macbookpro.local>
2024-06-02 04:56:52 +0800

    implement save the best model

--- training.py
+++ training.py
@@ -41,7 +41,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Initialize lists to store loss values
 train_losses = []
-
+best_loss = float('inf')
 num_epochs = 10
 for epoch in range(num_epochs):
     epoch_loss = 0
@@ -69,29 +69,34 @@ for epoch in range(num_epochs):
 
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_epoch_loss:.4f}')
 
-    # Save the training loss every 500 epochs
-    if (epoch + 1) % 500 == 0:
+    # Save the training loss every 1000 epochs
+    if (epoch + 1) % 1000 == 0:
         with open(folder_name + '/training_loss.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            if (epoch + 1) == 500:
+            if (epoch + 1) == 1000:
                 writer.writerow(['Epoch', 'Loss'])  # Write header only once
-            for e, loss in enumerate(train_losses[-500:]):
-                writer.writerow([epoch + 1 - 499 + e, loss])
+            for e, loss in enumerate(train_losses[-1000:]):
+                writer.writerow([epoch + 1 - 999 + e, loss])
 
-
-    if (epoch + 1) % 1000 == 0:
-        # Save the model checkpoint
+    # Save the model checkpoint
+    if (epoch + 1) % 2000 == 0:
         torch.save(model.state_dict(), folder_name + f'/model_epoch_{epoch+1}.pth')
+    # Save the model every 100 epochs if the loss is lower than the best loss
+    if (epoch + 1) % 100 == 0:
+        if avg_epoch_loss < best_loss:
+            best_loss = avg_epoch_loss
+            torch.save(model.state_dict(), folder_name + '/best_model.pth')
+            print(f'Model saved at epoch {epoch+1} with loss {avg_epoch_loss:.4f}')
 
 # Final save of remaining epochs if not a multiple of 500
-if num_epochs % 500 != 0:
-    remaining_epochs = num_epochs % 500
-    with open('training_loss.csv', 'a', newline='') as csvfile:
+if num_epochs % 1000 != 0:
+    remaining_epochs = num_epochs % 1000
+    with open(folder_name + '/training_loss.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        if num_epochs < 500:
+        if num_epochs < 1000:
             writer.writerow(['Epoch', 'Loss'])  # Write header if it hasn't been written
         for e, loss in enumerate(train_losses[-remaining_epochs:]):
             writer.writerow([num_epochs - remaining_epochs + e + 1, loss])
 
 # Save the final model
-torch.save(model.state_dict(), 'model_final.pth')
+torch.save(model.state_dict(), folder_name + '/model_final.pth')
