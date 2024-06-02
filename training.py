0f034da6fe64b4671571c3697500272c6250b381
Nam-Phuong Tran <namphuong@macbookpro.local>
2024-06-03 02:24:45 +0800

    implement learning rate scheduler

--- training.py
+++ training.py
@@ -37,12 +37,14 @@ num_classes = train_dataset.t_data.shape[1]
 # Initialize the model, define the loss function and the optimizer
 model = CNNSynchronizer(input_length, num_classes)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0005)
+# StepLR scheduler: reduce the learning rate by a factor of 0.1 every 1000 epochs
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.1)
 
 # Initialize lists to store loss values
 train_losses = []
 best_loss = float('inf')
-num_epochs = 10
+num_epochs = int(1e5)
 for epoch in range(num_epochs):
     epoch_loss = 0
     for inputs, labels in train_loader:
@@ -87,6 +89,8 @@ for epoch in range(num_epochs):
             best_loss = avg_epoch_loss
             torch.save(model.state_dict(), folder_name + '/best_model.pth')
             print(f'Model saved at epoch {epoch+1} with loss {avg_epoch_loss:.4f}')
+    # Step the scheduler
+    scheduler.step()
 
 # Final save of remaining epochs if not a multiple of 500
 if num_epochs % 1000 != 0:
