84808a3553fa5dfe022a724ab76c58612507d899
Nam-Phuong Tran <namphuong@macbookpro.local>
2024-06-28 02:46:55 +0900

    implement dropout

--- synchronizer.py
+++ synchronizer.py
@@ -12,6 +12,9 @@ class CNNSynchronizer(nn.Module):
         
         # Max pooling layer
         self.pool = nn.MaxPool1d(2)
+
+        # Dropout layer
+        self.dropout = nn.Dropout(0.5)
         
         # Calculate the size after Conv1D layers and pooling
         conv1_out_size = input_length - (3 - 1)
@@ -27,8 +30,10 @@ class CNNSynchronizer(nn.Module):
         x = F.relu(self.conv2(x))
         x = self.pool(F.relu(self.conv3(x)))
         x = self.pool(F.relu(self.conv4(x)))
+        x = self.dropout(x)
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
