e208ec55f54f47a19d16eeae0593f72e65191878
Nam-Phuong Tran <namphuong@macbookpro.local>
2024-06-29 22:09:56 +0900

    add batchnorm layer

--- synchronizer.py
+++ synchronizer.py
@@ -6,13 +6,20 @@ class CNNSynchronizer(nn.Module):
     def __init__(self, input_length, num_classes):
         super(CNNSynchronizer, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3)
+        self.bn1 = nn.BatchNorm1d(16)
+        
         self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(32)
+        
         self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(64)
+        
         self.conv4 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(128)
         
         # Max pooling layer
         self.pool = nn.MaxPool1d(2)
-
+        
         # Dropout layer
         self.dropout = nn.Dropout(0.5)
         
@@ -26,14 +33,13 @@ class CNNSynchronizer(nn.Module):
         self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
         x = self.dropout(x)
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
         return x
-
